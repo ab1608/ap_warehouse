c844677d0f31c6d2aabaf9794c14683dfc07de36
Abraham Briones <abebriones98@gmail.com>
2026-01-05 10:53:58 -0500

    test pipeline

--- test/test.py
+++ test/test.py
@@ -5,29 +5,23 @@ from pathlib import Path
 import duckdb
 from dotenv import load_dotenv
 
-from src.pipe import run_import
-from src.utils import list_files_by_extension
-
-
-def test_run_import(
-    db_conn: duckdb.DuckDBPyConnection, source_path: Path, data_format: str
-) -> None:
-    # Setup DuckDB connection
-    source_files = list_files_by_extension(source_path, data_format)
-
-    # Run the import function
-    run_import(db_conn, source_files)
-
-    # Clean up: close the connection
-    db_conn.close()
-
-
 project_root = Path(__file__).parent.parent
 sys.path.insert(0, str(project_root))
 env_path = project_root / ".env"
 load_dotenv(dotenv_path=env_path)
 database_path = os.getenv("DATABASE_PATH")
-project_path = os.getenv("PROJECT_PATH")
+output_path = os.getenv("OUTPUT_PATH")
+
+from src.pipe import FinancePipeline
+from src.utils import list_files_by_extension
 
 db_conn = duckdb.connect(str(database_path))
-test_run_import(db_conn, Path(str(project_path)) / "Inputs" / "TEST", "parquet")
+pipeline = FinancePipeline(db_conn)
+source_path = Path(".")
+data_files = list_files_by_extension(source_path, extension="csv")
+pipeline.run_import(data_files=data_files)
+pipeline.run_transformation(
+    str(output_path), range_start="2025/01/01", range_end="2025/02/01"
+)
+
+db_conn.close()
